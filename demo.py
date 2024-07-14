6a04315fd31008b6081cd805365e616e5caee1cd
Acceltra <acceltra521@gmail.com>
2024-07-14 21:44:39 +0800

    modify for running in local env

--- demo.py
+++ demo.py
@@ -16,6 +16,7 @@ if __name__ == "__main__":
   # frustums in the dataset
   # ======================================================================================================== #
   print("Estimating voxel volume bounds...")
+  #n_imgs = 1000
   n_imgs = 1000
   cam_intr = np.loadtxt("data/camera-intrinsics.txt", delimiter=' ')
   vol_bnds = np.zeros((3,2))
